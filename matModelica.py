3379d4c1393d21630d147f39c5c4f01762219707
Digvijay <42176718+dgusain1@users.noreply.github.com>
2018-08-22 11:51:16 +0200

    Can now be accessed only from terminal

--- matModelica.py
+++ matModelica.py
@@ -5,13 +5,23 @@ Created on Mon Aug 20 19:16:37 2018
 @author: digvijaygusain
 """
 
-#from lxml import etree
-#from fmpy.model_description import ScalarVariable
-#from scipy.io import loadmat
 import numpy as np
 import re
+import sys, os
+
+arguments = sys.argv
+modelicaFile, matlabFile = arguments[1], arguments[2]
+
+file_path = os.path.dirname(os.path.abspath(modelicaFile))
+modelica_model_name = modelicaFile.replace(file_path,'').split('.')[0][1:]+'_modified.mo'
+modified_file_path = os.path.join(file_path,modelica_model_name)
+
+
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#the following has been taken from StackOverflow answer:
+# https://stackoverflow.com/a/8832212
+
 import scipy.io as spio
 
 def loadmat(filename):
@@ -47,6 +57,7 @@ def _todict(matobj):
             dict_[strg] = elem
     return dict_
 
+#This has been developed by @dgusain1
 def check_regex(testString):
     #print(testString)
     voltage_regex = r'V_0\s?=\s?\d\.\d*'
@@ -57,7 +68,7 @@ def check_regex(testString):
     voltage = re.findall(voltage_regex, testString)[0].split(' = ')[1]
     angle = re.findall(angle_regex, testString)[0].split(' = ')[1]
     name = re.findall(r'([A-Za-z]+)(\d+)',re.findall(name_regex, testString)[0])
-    #print(name)
+    
     if len(name)>1:
         name = name[1]
     else:
@@ -67,17 +78,16 @@ def check_regex(testString):
     except IndexError:
         q='0'
     
-    #print([name, voltage, angle, q])
     return [name, voltage, angle, q]
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-
-#xmlFile = r'C:\Users\digvijaygusain\AppData\Local\Temp\OpenModelica\OMEdit\IEEE14BusWind_init.xml'
-matlabFile = r'D:\digvijaygusain\Desktop\result.mat'
+#matlabFile = r'D:\digvijaygusain\Desktop\result.mat'
 
 #load matlab results file
+print("Loading matlab result file...")
 matResult = loadmat(matlabFile)
 
+print("Organizing results..")
 #get results for bus and gen
 busResults = matResult['results']['bus']
 genResults = matResult['results']['gen']
@@ -87,25 +97,16 @@ genBuses = [int(x[0]) for x in genResults if x[1] !=0]
 busVoltagesTemp = [(int(x[0]), x[7], x[8]) for x in busResults]
 busVoltages = np.array(busVoltagesTemp, dtype=[('bus',np.int),('voltage',np.float),('angle',np.float)])
 
-#Create array for loads
-#loads must be named in sequence with their bus numbers
-#variables taken into account are Bus number, V_0, angle_0
-#temp = [(int(x[0]), x[7], x[8]) for x in busResults if x[2] != 0]
-#loadVariables = np.array(temp, dtype=[('bus',np.int),('voltage',np.float),('angle',np.float)])
-
 #Similarly create arrays for generators
 #variables are: bus number, voltage, angle, and Q_0
 temp = [(int(x[0]), x[2]) for x in genResults]
-#temp1 = []
-#for item in temp:
-#    index = list(busVoltages['bus']).index(item[0])
-#    temp1.append((item[0],busVoltages[index][1],busVoltages[index][2],item[1]))
 genVariables = np.array(temp, dtype=[('bus',np.int),('Q_0',np.float)])  #,('voltage',np.float),('angle',np.float)
 
 
 #input the *.mo file to be changed
-modelicaFile = r'C:\Users\digvijaygusain\OneDrive\TU_Delft\PhD\Teaching Assistantship\IEPG_Course_2019\Models\14 bus with wind\IEEE14Bus.mo'
-with open(modelicaFile) as infile, open(r'C:\Users\digvijaygusain\OneDrive\TU_Delft\PhD\Teaching Assistantship\IEPG_Course_2019\Models\14 bus with wind\IEEE14Bus_modified.mo', 'w') as outfile:
+#modelicaFile = r'C:\Users\digvijaygusain\OneDrive\TU_Delft\PhD\Teaching Assistantship\IEPG_Course_2019\Models\14 bus with wind\IEEE14Bus.mo'
+print("Initialising %s.mo.." %(modelica_model_name[:-12]))
+with open(modelicaFile) as infile, open(modified_file_path, 'w') as outfile:
     for line in infile:
         try:
             info = check_regex(line)
@@ -132,5 +133,5 @@ with open(modelicaFile) as infile, open(r'C:\Users\digvijaygusain\OneDrive\TU_De
         
 
         outfile.write(line)
-        
+print('Done..')
 
